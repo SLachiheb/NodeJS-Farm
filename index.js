abb146eb994941d463e406e0579e03b19ce3ffa3
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-01-06 11:15:32 +0100

    add module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const { replaceTemplate } = require('./modules/replaceTemplate.js');
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -15,24 +17,10 @@ const tempCard = fs.readFileSync(
   'utf-8'
 );
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(
-    /{%NOT_ORGANIC%}/g,
-    product.organic ? '' : 'not-organic'
-  );
-  return output;
-};
-
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const dataObject = JSON.parse(data);
+const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
